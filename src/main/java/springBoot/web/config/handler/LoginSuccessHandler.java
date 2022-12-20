094a542c499269713c4447c3b0b4be59d353d2be
andrew <vip.paevskiy@gmail.com>
2022-12-21 00:50:20 +0300

    Add project files

--- src/main/java/springBoot/web/config/handler/LoginSuccessHandler.java
+++ src/main/java/springBoot/web/config/handler/LoginSuccessHandler.java
@@ -1,7 +1,6 @@
 package springBoot.web.config.handler;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import springBoot.web.model.User;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Set;
 
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
