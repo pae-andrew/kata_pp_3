646e94f8abcb047ac2264eebe5b896987680fc21
andrew <vip.paevskiy@gmail.com>
2022-12-25 18:04:39 +0300

    Fix

--- src/main/java/springBoot/web/controller/RestUserController.java
+++ src/main/java/springBoot/web/controller/RestUserController.java
@@ -4,10 +4,7 @@ package springBoot.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springBoot.web.model.User;
 import springBoot.web.service.UserService;
 
@@ -17,38 +14,45 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/*")
+@RequestMapping("api/*")
 public class RestUserController {
 
     @Autowired
     private UserService userService;
 
+    @GetMapping
     @RequestMapping(value = "/admin/users", method = RequestMethod.POST)
     public ResponseEntity<List<User>> getAllUsers() {
+
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
+    @DeleteMapping
     @RequestMapping(value = "/admin/remove", method = RequestMethod.POST)
     public void removeUser(@RequestParam Long id) {
         userService.removeUser(id);
     }
 
+    @PutMapping
     @RequestMapping(value = "/admin/update", method = RequestMethod.POST)
     public void updateUser(@RequestParam Long id, String firstName, String password, String lastName, String email, int age, String role) {
         password = userService.ifPasswordNull(id, password);
         userService.updateUser(new User(id, firstName, lastName, age, email, password), role);
     }
 
+    @PostMapping
     @RequestMapping(value = "/admin/addUser", method = RequestMethod.POST)
     public void addUser(@RequestParam String firstName, String password, String lastName, String email, int age, String role) {
         userService.addUser(new User(firstName, password, lastName, email, age), role);
     }
 
+    @GetMapping
     @RequestMapping(value = "/user/getUser", method = RequestMethod.POST)
     public ResponseEntity<List<User>> getUser(HttpSession session) {
         List<User> userList = new ArrayList<>();
         User user = (User) session.getAttribute("user");
         userList.add(user);
+
         return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
